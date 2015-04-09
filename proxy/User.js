ba219d024cbf7c64840ca16bbb5ed4c725bed12b
coolicer <coolicer@foxmail.com>
2015-04-09 17:21:13 +0800

    修改user代理层

--- proxy/User.js
+++ proxy/User.js
@@ -1,14 +1,14 @@
 var Sequelize = require('sequelize');
 var User = require('../models').User;
 // var util = require('../lib/util');
+User.sync();
 
 /**
  * 根据用户ID，查找用户
  * @param {String} ID 用户ID
  * @param {Function} callback 回调函数
  */
-User.sync();
-
+ 
 module.exports = {
     getUserById: function (UserId ,callback) {
         User.findOne({
