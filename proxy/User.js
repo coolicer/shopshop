282f7be1a6029f937e2885523dd1c42b616c24d7
coolicer <coolicer@foxmail.com>
2015-04-09 17:22:01 +0800

    修改user代理层

--- proxy/User.js
+++ proxy/User.js
@@ -3,12 +3,6 @@ var User = require('../models').User;
 // var util = require('../lib/util');
 User.sync();
 
-/**
- * 根据用户ID，查找用户
- * @param {String} ID 用户ID
- * @param {Function} callback 回调函数
- */
- 
 module.exports = {
     getUserById: function (UserId ,callback) {
         User.findOne({
